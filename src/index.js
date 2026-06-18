89d454dedeeb28dfe54726b151d10d7e9bbdc48a
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-06-18 13:57:33 +0530

    one way of connection db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("rupesh kumar chaudhary")
+import mongoose  from "mongoose";
+import { DB_NAME } from "./constant";
+import express from "express";
+
+const app=express();
+
+
+
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}`);
+        app.on("Error",(err)=>{
+            console.log("Error:",err);
+            throw err;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening at ${process.env.PORT}`);
+        })
+    }catch(err){
+        console.log("Error:",err);
+        throw err;
+    }
+})()
