416b79036790a03d200e00112a33e9efadd4e58f
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-06-20 11:02:52 +0530

    Cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,18 @@ cloudinary.config({
   api_key:process.env.CLOUDINARY_API_KEY, 
   api_secret:process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary=async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("file is uploaded successfully",response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)//remove the locally saved temporarily saved file when the opereation is failed
+        return null;
+
+    }
+}
